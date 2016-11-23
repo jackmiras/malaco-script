a861fb0eff5148bab384d85323fe9d8020adad05
Jackson Miras <jackmiras@gmail.com>
2016-11-23 00:51:55 -0300

    Changing the way of how MysqlWorkBench are being installed

--- malaco-script.sh
+++ malaco-script.sh
@@ -190,9 +190,11 @@ fi
 
 if [ "$selected_item" == "14 - MySQL Workbench" ]; then
 	# Downloading MySQL Workbench.
-	wget -P $HOME/Download http://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
+	wget -P $HOME/Downloads/ http://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
 	# Installing MySQL Workbench.
-	cd $HOME/Download && sudo dpkg -i mysql-workbench-community-6.3.8-1ubu1604-amd64.deb && sudo apt-get -f install
+	sudo dpkg -i $HOME/Downloads/mysql-workbench-community-6.3.8-1ubu1604-amd64.deb && sudo apt-get -f install
+	# Removing mysql-workbench-community-6.3.8-1ubu1604-amd64.deb.
+	rm -rf $HOME/Downloads/mysql-workbench-community-6.3.8-1ubu1604-amd64.deb
 	# Updating Ubuntu dependencies.
 	sudo apt-get update && sudo apt-get upgrade
 fi
