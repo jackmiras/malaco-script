b15b789123b1df466a9972dad2eb3040863e29c9
Jackson Miras <jackmiras@gmail.com>
2016-11-29 11:04:02 -0300

    Changing the plugins to install git and git-flow

---
+++ plugins/git-flow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script written by: Jackson Miras
+#
+# This script install git-flow.
+
+sudo apt-get update && sudo apt-get install -y git-flow
--- plugins/git.sh
+++ plugins/git.sh
@@ -1,20 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 # Script written by: Jackson Miras.
-#
 # This script install git and configure git and also generate a ssh-key file.
 
-if which git >dev/null; then
-	# Installing git.                                                             
-	sudo apt-get update && sudo apt-get install -y git                            
+# Installing git.                                                             
+sudo apt-get update && sudo apt-get install -y git                            
 	                                                                                 
-	# Configuring git.                                                            
-	git config --global color.ui true                                             
-	git config --global user.name "Jackson Miras"                                 
-	git config --global user.email "jackmiras@gmail.com"                          
+# Configuring git.                                                            
+git config --global color.ui true                                             
+git config --global user.name "Jackson Miras"                                 
+git config --global user.email "jackmiras@gmail.com"                          
 	                                                                                 
-	# Generating ssh key.                                                         
-	ssh-keygen -t rsa -b 4096
-else
-	echo "Git is already installed... To this script take any effect you should uninstall git."
-fi
+# Generating ssh key.                                                         
+ssh-keygen -t rsa -b 4096
