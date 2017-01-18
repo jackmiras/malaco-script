cb2e4ff53f32c263a4938b53dbd2639272e33f1d
Jackson Miras <jackmiras@gmail.com>
2017-01-17 22:51:48 -0300

    Writing mysql installation plugin

---
+++ plugins/mysql-installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script written by: Jackson Miras.
+# This script install and configure Ruby, RVM and Rails.
+
+# Installing MySQL.
+	sudo apt-get update && sudo apt-get install mysql-server mysql-client libmysqlclient-dev
