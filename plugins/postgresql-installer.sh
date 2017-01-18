c27d1b6a6c1b380a42763bffe5b0b68fba8b9a89
Jackson Miras <jackmiras@gmail.com>
2017-01-17 22:53:10 -0300

    Writing postgresql installation plugin

---
+++ plugins/postgresql-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script written by: Jackson Miras.
+# This script install and configure PostgreSQL.
+
+# Adding PostgreSQL on Ubuntu souces.list.
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+# Downloading PostgreSQL key to add as key of system repositories.
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+# Installing PostgreSQL.
+sudo apt-get update && sudo apt-get install postgresql-common && sudo apt-get install postgresql-9.5 libpq-dev
