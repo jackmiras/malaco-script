ece359b651c4d6698422862dce137bb21891c665
Jackson Miras <jackmiras@gmail.com>
2017-01-17 22:56:16 -0300

    Plugin that install Google Chrome browser

---
+++ plugins/chrome-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script written by: Jackson Miras.
+# This script install Google Chrome browser.
+
+# Downloading Google Chrome key to add as key of system repositories.
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# Adding Google Chrome to the source list.
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+# Installing the stable version of Google Chrome.
+sudo apt-get update && sudo apt-get install -y google-chrome-stable && sudo apt-get -f install
