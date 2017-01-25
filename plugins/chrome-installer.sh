0fef052e1e37ffe7cb38fda1e893de46f6c117a7
Jackson Miras <jackmiras@gmail.com>
2017-01-24 21:55:33 -0300

    Updating the comments of chrome-installer.sh script

--- plugins/chrome-installer.sh
+++ plugins/chrome-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script written by: Jackson Miras.
+# Script written by: Jack Miras.
 # This script install Google Chrome browser.
 
 # Downloading Google Chrome key to add as key of system repositories.
