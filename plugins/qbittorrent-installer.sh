3bb986e21f355f176961ae129d6f55e5868bac25
Jackson Miras <jackmiras@gmail.com>
2017-01-24 22:00:57 -0300

    Creating the plugin that install qBittorrent

---
+++ plugins/qbittorrent-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script written by: Jack Miras.
+# This script install qBittorrent.
+
+# Adding qBittorrent repository.
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+
+# Installing qBittorrent.
+sudo apt-get update && sudo apt-get install qbittorrent
+
