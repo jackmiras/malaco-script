51adf1bf1839619628a04c5aab95290b56186689
Jackson Miras <jackmiras@gmail.com>
2017-01-30 23:19:01 -0300

    Changing the installation of GIMP

--- malaco-script.sh
+++ malaco-script.sh
@@ -466,9 +466,9 @@ fi
 
 if [[ "$selected_item" =~ "32" ]]; then
 	# Setting up ppa.
-	sudo add-apt-repository ppa:otto-kesselgulasch/gimp-edge && sudo apt-get update
+	sudo add-apt-repository ppa:otto-kesselgulasch/gimp && sudo apt-get update
 	# Installing Gimp.
-	sudo apt-get -y install gimp
+	sudo apt-get install gimp
 fi
 
 if [[ "$selected_item" =~ "33" ]]; then
