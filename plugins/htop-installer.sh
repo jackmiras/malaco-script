1716ac6abbed2c7a631cb37ed30e96b35f4eca3c
Jackson Miras <jackmiras@gmail.com>
2017-01-31 23:15:49 -0300

    Checking if Htop isn't installed before try to install him

--- plugins/htop-installer.sh
+++ plugins/htop-installer.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-#	Script written by: Jackson Miras.
+#	Script written by: JM.
 # This script install the htop software.
 
-#Installing Htop.
-sudo apt-get update && sudo apt-get install -y htop
+if command -v htop >/dev/null; then
+	echo 'Htop is already installed.'
+else
+	#Installing Htop.
+	sudo apt-get update && sudo apt-get install -y htop
+	echo $'\nHtop was installed with success.'
+fi
