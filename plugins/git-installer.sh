e9020f43cb0d036dca0b7a1a53870ad9e44a1795
Jackson Miras <jackmiras@gmail.com>
2017-01-31 22:48:12 -0300

    Checking if git exists before try to install him

--- plugins/git-installer.sh
+++ plugins/git-installer.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Script written by: Jackson Miras.
+# Script written by: JM.
 # This script install git and configure git and also generate a ssh-key file.
 
-# Installing git.                                                             
-sudo apt-get update && sudo apt-get install -y git                            
+if command -v git >/dev/null; then
+	echo "Git is already installed."
+else
+	# Installing git.                                                             
+	sudo apt-get update && sudo apt-get install -y git                            
 
-# Configuring git.                                                            
-git config --global color.ui true                                             
-git config --global user.name "Jackson Miras"                                 
-git config --global user.email "jackmiras@gmail.com"
+	# Configuring git.                                                            
+	git config --global color.ui true                                             
+	git config --global user.name "Jackson Miras"                                 
+	git config --global user.email "jackmiras@gmail.com"
 
-# Generating ssh key.                                                         
-ssh-keygen -t rsa -b 4096
+	# Generating ssh key.                                                         
+	ssh-keygen -t rsa -b 4096
+	echo "Git was installed with success."
+fi
