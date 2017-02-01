3031f028686229ce69fff5a143eeafcc93f83b66
Jackson Miras <jackmiras@gmail.com>
2017-01-31 22:54:19 -0300

    Checking if Git flow exists before try to install him

--- plugins/git-flow-installer.sh
+++ plugins/git-flow-installer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Script written by: Jackson Miras
+# Script written by: JM.
 # This script install git-flow.
 
-sudo apt-get update && sudo apt-get install -y git-flow
+if command -v git flow >/dev/null; then
+	echo 'Git flow is already installed.'
+else
+	sudo apt-get update && sudo apt-get install -y git-flow
+	echo $'\nGit flow was installed with succes.'
+fi
