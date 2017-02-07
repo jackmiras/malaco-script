332a1baa4f216c220aee6dd7cbecb8d8d1e541e1
Jackson Miras <jackmiras@gmail.com>
2017-02-06 21:56:31 -0300

    Checking if MySQL isn't installer before try to install him.

--- plugins/mysql-installer.sh
+++ plugins/mysql-installer.sh
@@ -2,5 +2,10 @@
 # Script written by: Jackson Miras.
 # This script install and configure Ruby, RVM and Rails.
 
-# Installing MySQL.
-	sudo apt-get update && sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+if command -v mysql >/dev/null; then
+  echo 'MySQL is already installed.'
+else
+  # Installing MySQL.
+  sudo apt-get update && sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+  echo $'\nMysql was installed with success!'
+fi
