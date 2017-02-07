74434c364320490e5e52baba5316e8a1e8407b69
Jackson Miras <jackmiras@gmail.com>
2017-02-06 23:04:19 -0300

    Checking if PostgreSQL is installed before try to install him.

--- plugins/postgresql-installer.sh
+++ plugins/postgresql-installer.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# Script written by: Jackson Miras.
+# Script written by: JM.
 # This script install and configure PostgreSQL.
 
-# Adding PostgreSQL on Ubuntu souces.list.
-sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
-# Downloading PostgreSQL key to add as key of system repositories.
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-# Installing PostgreSQL.
-sudo apt-get update && sudo apt-get install postgresql-common && sudo apt-get install postgresql-9.5 libpq-dev
+if command -v psql >/dev/null; then
+  echo 'PostgreeSQL is already installed.'
+else
+  # Adding PostgreSQL on Ubuntu souces.list.
+  sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+  # Downloading PostgreSQL key to add as key of system repositories.
+  wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+  # Installing PostgreSQL.
+  sudo apt-get update && sudo apt-get install postgresql-common && sudo apt-get install postgresql-9.5 libpq-dev
+  echo $'\nPostgreeSQL was installed with success!'
+fi
