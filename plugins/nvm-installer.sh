c9d82c751bdad46e848a4a153b0633737035baa3
Jackson Miras <jackmiras@gmail.com>
2017-02-06 21:28:38 -0300

    Checking if NVM isn't installed before try to install him.

--- plugins/nvm-installer.sh
+++ plugins/nvm-installer.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Script written by: Jackson Miras.
+# Script written by: JM.
 # This script install and configure NVM(Node Package Manager).
 
-# Installing Node build essential.
-sudo apt-get update && sudo apt-get install build-essential libssl-dev
-# Downloading nvm script.
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
-# Installing nvm.
-bash install_nvm.sh
+if [ -d "$HOME/.nvm/" ]; then
+  echo 'NVM is already installed.'
+else
+  # Installing Node build essential.
+  sudo apt-get update && sudo apt-get install build-essential libssl-dev
+  # Downloading nvm script.
+  curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+  # Installing nvm.
+  bash install_nvm.sh
 
-zenity --info --text="Close and reopen the terminal."
+  zenity --info --text="Close and reopen the terminal."
 
+  echo 'NVM was installed with success!'
+fi
