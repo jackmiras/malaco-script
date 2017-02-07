b32442253c16a4ea91afc2f3fc922d1cfa73d4c8
Jackson Miras <jackmiras@gmail.com>
2017-02-06 21:34:15 -0300

    Checking if Node isn't installed before try to install him.

--- plugins/node-installer.sh
+++ plugins/node-installer.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Script written by: Jackson Miras.
+# Script written by: JM.
 # This script install and configure Node.
 
-# Enabling the nvm.sh script to run in bash scripts.
-. ~/.nvm/nvm.sh
-. ~/.profile
-. ~/.bashrc
-. ~/.zshrc
+if command -v npm >/dev/null; then
+  echo 'NPM is already installed.'
+else
+  # Enabling the nvm.sh script to run in bash scripts.
+  . ~/.nvm/nvm.sh
+  . ~/.profile
+  . ~/.bashrc
+  . ~/.zshrc
+  # Installing Node.
+  nvm install 7.2.0
+  nvm use 7.2.0
+  nvm alias default 7.2.0
+  # Intalling npm.
+  sudo apt-get install npm
 
-# Installing Node.
-nvm install 7.2.0
-nvm use 7.2.0
-nvm alias default 7.2.0
-
-# Intalling npm.
-sudo apt-get install npm
+  echo $'\nNPM was isntalled with success!'
+fi
