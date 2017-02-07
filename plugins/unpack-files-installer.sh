53efca6ce4043f3889c1603256a7311a9d57686b
Jackson Miras <jackmiras@gmail.com>
2017-02-06 21:01:34 -0300

    Checking if unpack software aren't installed before try to install them.

--- plugins/unpack-files-installer.sh
+++ plugins/unpack-files-installer.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-# Script written by: Jackson Miras.
+# Script written by: JM.
 # This script install softwares that are resposible by unpack files.
 
-# Installing unpack files software.
-sudo apt-get -y install p7zip p7zip-full p7zip-rar unrar rar unace-nonfree
+########################### Installing p7zip ###################################
+if command -v p7zip >/dev/null; then
+  echo ' p7zip is already installed.'
+else
+  sudo apt-get -y install p7zip p7zip-full p7zip-rar
+  echo $'\n p7zip was installed with success!'
+fi
+
+########################## Installing unrar ####################################
+if command -v unrar >/dev/null; then
+  echo $'\n Unrar is already installed.'
+else
+  sudo apt-get -y unrar
+  echo $'\n Unrar was installed with success!'
+fi
+
+########################## Installing unrar ####################################
+if command -v rar >/dev/null; then
+  echo $'\n Rar is already installed.'
+else
+  sudo apt-get -y rar
+  echo $'\n Rar was installed with success!'
+fi
+
+########################## Installing unace ####################################
+if command -v unace >/dev/null; then
+  echo $'\n Unace-nonfree is already installed.'
+else
+  sudo apt-get -y unace-nonfree
+  echo $'\n Unace-nonfree was installed with success!'
+fi
+
