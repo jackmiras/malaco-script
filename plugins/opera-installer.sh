a5a3c00e1318da598ec6284c83a693516fb59017
Jackson Miras <jackmiras@gmail.com>
2017-02-09 22:35:02 -0300

    Checing if  Opera isn't installed before try to install him

--- plugins/opera-installer.sh
+++ plugins/opera-installer.sh
@@ -2,12 +2,16 @@
 # Script written by: Jack Miras.
 # This script install Opera browser.
 
-# Downloading opera-stable_41.0.2353.46_amd64.deb into the Download folder.
-wget -P $HOME/Downloads/ http://download4.operacdn.com/pub/opera/desktop/41.0.2353.46/linux/opera-stable_41.0.2353.46_amd64.deb
+if command -v opera >/dev/null; then
+  echo 'Opera is already installed.'
+else
+  # Downloading opera-stable_41.0.2353.46_amd64.deb into the Download folder.
+  wget -P $HOME/Downloads/ http://download4.operacdn.com/pub/opera/desktop/41.0.2353.46/linux/opera-stable_41.0.2353.46_amd64.deb
 
-# Installing the stable version Opera.
-sudo dpkg -i $HOME/Downloads/opera-stable_41.0.2353.46_amd64.deb && sudo apt-get -f install
+  # Installing the stable version Opera.
+  sudo dpkg -i $HOME/Downloads/opera-stable_41.0.2353.46_amd64.deb && sudo apt-get -f install
 
-# Updating Ubuntu dependencies. 
-sudo ap-get update && sudo apt-get upgrade
+  # Updating Ubuntu dependencies. 
+  sudo ap-get update && sudo apt-get upgrade
+fi
 
