4518a05a8dccc53fac39696c4881354ab30db4ae
Jackson Miras <jackmiras@gmail.com>
2017-02-09 22:14:54 -0300

    Checking if Sublime isn't install before try to install him

--- plugins/sublime-text3-installer.sh
+++ plugins/sublime-text3-installer.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# Script written by: Jackson Miras.
+# Script written by: JM.
 # This script install android configure Sublime Text 3 text editor.
 
-# Adding Sublime Text 3 respository.
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 && sudo apt-get update
+if command -v subl >/dev/null; then
+  echo 'Sublime is already installed.'
+else
+  # Adding Sublime Text 3 respository.
+  sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 && sudo apt-get update
 
-# Installing Sublime Text 3.
-sudo apt-get -y install sublime-text-installer
+  # Installing Sublime Text 3.
+  sudo apt-get -y install sublime-text-installer
 
-# Downloading Sublime Text 3 configs from Github.
-if [ ! -d "$HOME/Projects/sublime-text-3-config" ]; then
-		cd $HOME/Projects && git clone git@github.com:jackmiras/sublime-text-3-config.git && cd $HOME
+  # Downloading Sublime Text 3 configs from Github.
+  if [ ! -d "$HOME/Projects/sublime-text-3-config" ]; then
+	  cd $HOME/Projects && git clone git@github.com:jackmiras/sublime-text-3-config.git && cd $HOME
+  fi
+
+  # Creating a symbolic link of Sublime Text 3 configs into the Home/.config folder of Ubuntu.
+  ln -s $HOME/Projects/sublime-text-3-config $HOME/.config/sublime-text-3
 fi
-
-# Creating a symbolic link of Sublime Text 3 configs into the Home/.config folder of Ubuntu.
-ln -s $HOME/Projects/sublime-text-3-config $HOME/.config/sublime-text-3
 
