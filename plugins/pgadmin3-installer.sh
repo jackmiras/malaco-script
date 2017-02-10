7056dc52e0eaeb5619fbf0c555bc98a5c9e1714d
Jackson Miras <jackmiras@gmail.com>
2017-02-09 22:06:11 -0300

    Checking if pgadmin3 isn't installed before try to install him.

--- plugins/pgadmin3-installer.sh
+++ plugins/pgadmin3-installer.sh
@@ -2,5 +2,10 @@
 # Script written by: Jackson Miras.
 # This script install and configure PgAdmin3.
 
-# Installing PgAdmin3.
-sudo apt-get update && sudo apt-get install pgadmin3
+if command -v pgadmin3 >/dev/null; then
+  echo 'PostgreeSQL is already installed.'
+else
+  # Installing PgAdmin3.
+  sudo apt-get update && sudo apt-get install pgadmin3
+fi
+
