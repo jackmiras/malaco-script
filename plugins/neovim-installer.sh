fe338d9323dfd5132cc5f334af51d6cccbcfeaf8
Jackson Miras <jackmiras@gmail.com>
2017-02-09 22:09:28 -0300

    Checking if Neovim isn't install before try to install him

--- plugins/neovim-installer.sh
+++ plugins/neovim-installer.sh
@@ -2,24 +2,29 @@
 # Script written by: Jackson Miras.
 # This script install and configure Vim text editor.
 
-# Installing xclip as a dependency required by Neovim
-sudo apt-get install xclip
+if command -v nvim >/dev/null; then
+  echo 'Neovim is already installed.'
+else
+  # Installing xclip as a dependency required by Neovim
+  sudo apt-get install xclip
 
-# Installing Neovim.
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+  # Installing Neovim.
+  sudo apt-get install software-properties-common
+  sudo apt-get install python-software-properties
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update
+  sudo apt-get install neovim
 
-if [ ! -d "$HOME/Projects/neovim-config" ]; then
-	# Downloading Neovim configs from github.
-	cd $HOME/Projects && git clone git@github.com:jackmiras/neovim-config.git && cd $HOME
+  if [ ! -d "$HOME/Projects/neovim-config" ]; then
+	  # Downloading Neovim configs from github.
+	  cd $HOME/Projects && git clone git@github.com:jackmiras/neovim-config.git && cd $HOME
+  fi
+
+  # Loading the Neovim plugins
+  cd $HOME/Projects/neovim-config
+  git submodule init && git submodule update --recursive
+
+  # Creating a symbolic link of Neovim configurations into the .config folder of Ubuntu.
+  ln -s $HOME/Projects/neovim-config $HOME/.config/nvim
 fi
 
-# Loading the Neovim plugins
-cd $HOME/Projects/neovim-config
-git submodule init && git submodule update --recursive
-
-# Creating a symbolic link of Neovim configurations into the .config folder of Ubuntu.
-ln -s $HOME/Projects/neovim-config $HOME/.config/nvim
