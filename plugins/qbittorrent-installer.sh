095a680d0808d5f20ccd93ad69b78a68330ff22c
Jackson Miras <jackmiras@gmail.com>
2017-02-09 22:58:09 -0300

    Checking if qBittorrent isn't installed before try to install him

--- plugins/qbittorrent-installer.sh
+++ plugins/qbittorrent-installer.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# Script written by: Jack Miras.
+# Script written by: JM.
 # This script install qBittorrent.
 
-# Adding qBittorrent repository.
-sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+if command -v qbittorrent >/dev/null; then
+  echo 'qBittorrent is already installed.'
+else
+  # Adding qBittorrent repository.
+  sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
 
-# Installing qBittorrent.
-sudo apt-get update && sudo apt-get install qbittorrent
+  # Installing qBittorrent.
+  sudo apt-get update && sudo apt-get install qbittorrent
+fi
 
