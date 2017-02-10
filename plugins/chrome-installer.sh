a5552db0556c1b7c8eb35d2b68db201e617d0821
Jackson Miras <jackmiras@gmail.com>
2017-02-09 22:31:51 -0300

    Checing if Chrome isn't installed before try to install him

--- plugins/chrome-installer.sh
+++ plugins/chrome-installer.sh
@@ -2,11 +2,16 @@
 # Script written by: Jack Miras.
 # This script install Google Chrome browser.
 
-# Downloading Google Chrome key to add as key of system repositories.
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+if command -v google-chrome >/dev/null; then
+  echo 'Google chrome is already install.'
+else
+  # Downloading Google Chrome key to add as key of system repositories.
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-# Adding Google Chrome to the source list.
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+  # Adding Google Chrome to the source list.
+  sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
-# Installing the stable version of Google Chrome.
-sudo apt-get update && sudo apt-get install -y google-chrome-stable && sudo apt-get -f install
+  # Installing the stable version of Google Chrome.
+  sudo apt-get update && sudo apt-get install -y google-chrome-stable && sudo apt-get -f install
+fi
+
