ba48ebaf09d2f40b92a896af28322679a5a0b14e
Jackson Miras <jackmiras@gmail.com>
2017-02-10 22:35:08 -0300

    Checking if Spotify ins't installed before try to install him.

--- plugins/spotify-install.sh
+++ plugins/spotify-install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# Script written by: Jack Miras.
+# Script written by: JM.
 # This script install Spotfy.
 
-# Downloading Spotfy client.
-wget -P $HOME/Downloads/ http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.38.171.g5e1cd7b2-22_amd64.deb
+if command -v spotify >/dev/null; then
+  echo 'Spotify is already installed.'
+else
+  # Downloading Spotfy client.
+  wget -P $HOME/Downloads/ http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.48.103.g15edf1ec-121_amd64.deb
 
-# Installing Spotfy client.
-sudo dpkg -i spotify-client_1.0.38.171.g5e1cd7b2-22_amd64.deb && sudo apt-get -f install
+  # Installing Spotfy client.
+  sudo dpkg -i $HOME/Downloads/spotify-client_1.0.48.103.g15edf1ec-121_amd64.deb && sudo apt-get -f install
 
-# Updating Ubuntu dependencies.
-sudo apt-get install && sudo apt-get upgrade
+  # Updating Ubuntu dependencies.
+  sudo apt-get install && sudo apt-get upgrade
 
-# Removing Spotfy file from Download folder.
-rm -rf $HOME/Downloads/spotify-client_1.0.38.171.g5e1cd7b2-22_amd64.deb
+  # Removing Spotfy file from Download folder.
+  rm -rf $HOME/Downloads/spotify-client_1.0.48.103.g15edf1ec-121_amd64.deb
+fi
