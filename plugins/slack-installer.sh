4330ce1526f5a66a1bd54b54ee6e97396c91438d
Jackson Miras <jackmiras@gmail.com>
2017-02-11 11:05:43 -0300

    Checking if Slack isn't installed before try to install him.

--- plugins/slack-installer.sh
+++ plugins/slack-installer.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# Script written by: Jack Miras.
+# Script written by: JM.
 # This script install Slack.
 
-# Downloading Slack.
-wget -P $HOME/Downloads/ https://downloads.slack-edge.com/linux_releases/slack-desktop-2.2.1-amd64.deb
+if command -v slack >/dev/null; then
+  echo 'Slack is already installed.'
+else
+https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb
+  # Downloading Slack.
+  wget -P $HOME/Downloads/ https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb
 
-# Installing Slack.
-sudo dpkg -i $HOME/Downloads/slack-desktop-2.2.1-amd64.deb
+  # Installing Slack.
+  sudo dpkg -i $HOME/Downloads/slack-desktop-2.4.2-amd64.deb && sudo apt-get -f install
 
-# Installing Slack dependencies.
-sudo apt-get -f install
+  # Removing slack-desktop-2.2.1-amd64.deb file.
+  rm -rf $HOME/Downloads/slack-desktop-2.4.2-amd64.deb
 
-# Removing slack-desktop-2.2.1-amd64.deb file.
-rm -rf $HOME/Downloads/slack-desktop-2.2.1-amd64.deb
+  # Updating Ubuntu softwares and dependencies.
+  sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove
+fi
 
-# Updating Ubuntu softwares and dependencies.
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove
