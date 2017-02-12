71a8c62743a8b096bdf451bd27d75681dbcfb186
Jackson Miras <jackmiras@gmail.com>
2017-02-12 13:07:17 -0300

    Checking is Linuxbrew isn't installed before try to install him.

--- plugins/linuxbrew-installer.sh
+++ plugins/linuxbrew-installer.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# Script written by: Jack Miras.
+# Script written by: JM.
 # This script install Linuxbrew.
 
-# Installing Linuxbrew dependencies.
-sudo apt-get install build-essential curl git python-setuptools ruby
+if command -v brew >/dev/null; then
+  echo 'Linuxbrew is already installed.'
+else
+  # Installing Linuxbrew dependencies.
+  sudo apt-get install build-essential curl git python-setuptools ruby
 	
-# Installing Linuxbrew.
-git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+  # Installing Linuxbrew.
+  git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
 	
-# Exporting environment variables of Linuxbrew.
-echo "export PATH='$HOME/.linuxbrew/bin:$PATH'" >> ~/.bashrc
-echo "export MANPATH='$HOME/.linuxbrew/share/man:$MANPATH'" >> ~/.bashrc
-echo "export INFOPATH='$HOME/.linuxbrew/share/info:$INFOPATH'" >> ~/.bashrc
+  # Exporting environment variables of Linuxbrew.
+  echo "export PATH='$HOME/.linuxbrew/bin:$PATH'" >> ~/.bashrc
+  echo "export MANPATH='$HOME/.linuxbrew/share/man:$MANPATH'" >> ~/.bashrc
+  echo "export INFOPATH='$HOME/.linuxbrew/share/info:$INFOPATH'" >> ~/.bashrc
+fi
+
