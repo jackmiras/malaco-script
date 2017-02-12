3c532dd2b8d3524fd98269e1800406be717e35cc
Jackson Miras <jackmiras@gmail.com>
2017-02-12 09:47:05 -0300

    Checking if Simplenote isn't installed before try to install him.

--- plugins/simplenote-installer.sh
+++ plugins/simplenote-installer.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-# Script written by: Jack Miras.
+# Script written by: JM.
 # This script install Simplenote.
 
-# Downloading Simplenote.
-wget -P $HOME/Downloads/ https://github.com/Automattic/simplenote-electron/releases/download/v1.0.6/simplenote-1.0.6.deb
-	
-# Installing Simplenote.
-sudo dpkg -i $HOME/Downloads/simplenote-1.0.6.deb && sudo apt-get -f install
+if [ -d "/usr/share/simplenote/" ]; then
+  echo 'Simplenote is already installed.'
+else
+  # Downloading Simplenote.
+  wget -P $HOME/Downloads/ https://github.com/Automattic/simplenote-electron/releases/download/v1.0.8/simplenote-1.0.8.deb
 
-# Removing simplenote-1.0.6.deb file.
-rm -rf $HOME/Downloads/simplenote-1.0.6.deb
 	
-# Updating Ubuntu softwares and dependencies.
-sudo apt-get update && sudo apt-get upgrade
+  # Installing Simplenote.
+  sudo dpkg -i $HOME/Downloads/simplenote-1.0.8.deb && sudo apt-get -f install
+
+  # Removing simplenote-1.0.6.deb file.
+  rm -rf $HOME/Downloads/simplenote-1.0.8.deb
+	
+  # Updating Ubuntu softwares and dependencies.
+  sudo apt-get update && sudo apt-get upgrade
+fi
