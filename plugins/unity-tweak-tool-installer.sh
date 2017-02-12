6fa847a25594ce975b3dc3f2f3d027fd1016bf43
Jackson Miras <jackmiras@gmail.com>
2017-02-12 12:47:44 -0300

    Checking if Unity Tweak Tool isn't installed before try to install him.

--- plugins/unity-tweak-tool-installer.sh
+++ plugins/unity-tweak-tool-installer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# Script written by: Jack Miras.
+# Script written by: JM.
 # This script install Unity Tweak Tool.
 
-# Installing Unity Tweek Tool
-sudo apt-get update && sudo apt-get install unity-tweak-tool
+if command -v unity-tweak-tool >/dev/null; then
+  echo 'Unity Tweak tool is already installed.'
+else
+  # Installing Unity Tweek Tool
+  sudo apt-get update && sudo apt-get install unity-tweak-tool
+fi
+
