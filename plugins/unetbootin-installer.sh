52fda1b2e0983865e5ddda753c0cb131575a575d
Jackson Miras <jackmiras@gmail.com>
2017-02-12 12:45:33 -0300

    Checking if Unetbootin isn't installed before try to install him.

--- plugins/unetbootin-installer.sh
+++ plugins/unetbootin-installer.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# Script written by: Jack Miras.
+# Script written by: JM.
 # This script install Unetbootin.
 
-# Adding Unetbootin repository.
-sudo add-apt-repository ppa:gezakovacs/ppa
+if command -v unetbootin >/dev/null; then
+  echo 'Unetbootin is already installed.'
+else
+  # Adding Unetbootin repository.
+  sudo add-apt-repository ppa:gezakovacs/ppa
 	
-# Installing Unetbootin
-sudo apt-get update && sudo apt-get install unetbootin
+  # Installing Unetbootin
+  sudo apt-get update && sudo apt-get install unetbootin
+fi
+
