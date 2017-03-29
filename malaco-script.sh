c3feb7ee2b9a3b895fa9f535c0b0eebef9ed1971
Jackson Miras <jackmiras@gmail.com>
2017-03-29 10:28:51 -0400

    Removing unnecessary comments from malaco-script.sh file

--- malaco-script.sh
+++ malaco-script.sh
@@ -58,239 +58,193 @@ selected_item=$(zenity --list\
 )
 
 if [[ "$selected_item" =~ "1" ]]; then
-	# Installing git.
   ./plugins/git-installer.sh
 fi
 
 if [[ "$selected_item" =~ "2" ]]; then
-	# Installing Git-flow.
   ./plugins/git-flow-installer.sh
 fi
 
 if [[ "$selected_item" =~ "3" ]]; then
-	# Getting repositories from Github and Bitbucket.
   ./plugins/repositories-github-bitbucket.sh
 fi
 
 if [[ "$selected_item" =~ "4" ]]; then
-	# Installing Htop.
   ./plugins/htop-installer.sh
 fi
 
 if [[ "$selected_item" =~ "5" ]]; then
-	# Installing Tmux.
   ./plugins/tmux-installer.sh
 fi
 
 if [[ "$selected_item" =~ "6" ]]; then
-	# Installing zsh.
   ./plugins/zsh-installer.sh
 fi
 
 if [[ "$selected_item" =~ "7" ]]; then
-	# Installing Zsh Syntax Highlighting.
   ./plugins/zsh-syntax-highlighting-installer.sh
 fi
 
 if [[ "$selected_item" =~ "8" ]]; then
-	# Installing unpack files software.
   ./plugins/unpack-files-installer.sh
 fi
 
 if [[ "$selected_item" =~ "9" ]]; then
-	# Installing Oracle Java 8.
 	./plugins/java8-installer.sh
 fi
 
 if [[ "$selected_item" =~ "10" ]]; then
-	# Installing NVM.
   ./plugins/nvm-installer.sh 
 fi
 
 if [[ "$selected_item" =~ "11" ]]; then
-	# Installing Node.
 	./plugins/node-installer.sh
 fi
 
 if [[ "$selected_item" =~ "12" ]]; then
-	# Installing Ruby.
   ./plugins/ruby-installer.sh
 fi
 
 if [[ "$selected_item" =~ "13" ]]; then
-	# Installing MySQL.
   ./plugins/mysql-installer.sh
 fi
 
 if [[ "$selected_item" =~ "14" ]]; then
-	# Installing MySQL Workbench.
 	./plugins/mysql-workbench-installer.sh
 fi
 
 if [[ "$selected_item" =~ "15" ]]; then
-	# Installing PostgreSQL.
   ./plugins/postgresql-installer.sh	
 fi
 
 if [[ "$selected_item" =~ "16" ]]; then
-	# Installing PgAdmin3.
   ./plugins/pgadmin3-installer.sh
 fi
 
 if [[ "$selected_item" =~ "17" ]]; then
-  # Installing Neovim.
 	./plugins/neovim-installer.sh
 fi
 
 if [[ "$selected_item" =~ "18" ]]; then
-	# Installing Sublime Text 3.
 	./plugins/sublime-text3-installer.sh
 fi
 
 if [[ "$selected_item" =~ "19" ]]; then
-	# Installing Android Studio.
 	./plugins/android-studio-installer.sh
 fi
 
 if [[ "$selected_item" =~ "20" ]]; then
-	# Installing Eclipse.
   ./plugins/eclipse-installer.sh
 fi
 
 if [[ "$selected_item" =~ "21" ]]; then	
-	# Installing Google Chrome.
   ./plugins/chrome-installer.sh
 fi
 
 if [[ "$selected_item" =~ "22" ]]; then
-	# Installing Opera.
   ./plugins/opera-installer.sh
 fi
 
 if [[ "$selected_item" =~ "23" ]]; then
-	# Installing Dropbox.
 	./plugins/dropbox-installer.sh
 fi
 
 if [[ "$selected_item" =~ "24" ]]; then
-	# Installing pCloud.
   ./plugins/pcloud-installer.sh
 fi
 
 if [[ "$selected_item" =~ "25" ]]; then
-	# Installing qBittorrent.
   ./plugins/qbittorrent-installer.sh
 fi
 
 if [[ "$selected_item" =~ "26" ]]; then
-	# Installing Spotfy client.
   ./plugins/spotify-install.sh
 fi
 
 if [[ "$selected_item" =~ "27" ]]; then
-	# Installing Clementine.
   ./plugins/clementine-installer.sh
 fi
 
 if [[ "$selected_item" =~ "28" ]]; then	
-	# Installing Vocal.
 	./plugins/vocal-installer.sh
 fi
 
 if [[ "$selected_item" =~ "29" ]]; then
-	# Installing VLC.
   ./plugins/vlc-installer.sh
 fi
 
 if [[ "$selected_item" =~ "30" ]]; then
-	# Installing Stremio
   ./plugins/stremio-installer.sh
 fi
 
 if [[ "$selected_item" =~ "31" ]]; then
-	# Installing Inkscape.
   ./plugins/inkscape-installer.sh
 fi
 
 if [[ "$selected_item" =~ "32" ]]; then
-	# Installing Slack.
 	./plugins/slack-installer.sh
 fi
 
 if [[ "$selected_item" =~ "33" ]]; then
-	# Installing Skype.
 	./plugins/skype-installer.sh
 fi
 
 if [[ "$selected_item" =~ "34" ]]; then
-	# Installing Simplenote.
 	./plugins/simplenote-installer.sh
 fi
 
 if [[ "$selected_item" =~ "35" ]]; then
-	# Installing Unetbootin
   ./plugins/unetbootin-installer.sh
 fi
 
 if [[ "$selected_item" =~ "36" ]]; then
-	# Installing Unity Tweek Tool
   ./plugins/unity-tweak-tool-installer.sh
 fi
 
 if [[ "$selected_item" =~ "37" ]]; then
-	# Installing Restricted-Extras.
   ./plugins/ubuntu-restricted-extras.sh
 fi
 
 if [[ "$selected_item" =~ "38" ]]; then
-	# Installing Syspeek.
   ./plugins/syspeek-installer.sh
 fi
 
 if [[ "$selected_item" =~ "39" ]]; then
-	# Installing Linuxbrew.
 	./plugins/linuxbrew-installer.sh
 fi
 
-#################Uninstalling unused softwares from Ubuntu.##################
+##################Uninstalling unused softwares from Ubuntu.####################
 if command -v onboard >/dev/null; then
-# Removing Onboard from the system.
 	sudo apt-get remove --purge onboard
 	sudo apt-get autoremove
 fi
 
 if command -v gnome-mahjongg >/dev/null; then
-	# Removing GNOME Mahjongg.
 	sudo apt-get remove --purge gnome-mahjongg
 	sudo apt-get autoremove
 fi
 
 if command -v gnome-sudoku >/dev/null; then
-	# Removing GNOME Sudoku.
 	sudo apt-get remove --purge gnome-sudoku
 	sudo apt-get autoremove
 fi
 
 if command -v gnome-orca >/dev/null; then
-	# Removing GNOME Orca.
 	sudo apt-get remove --purge gnome-orca
 	sudo apt-get autoremove
 fi
 
 if command -v gnome-mines>/dev/null; then
-	# Removing GNOME Mines.
 	sudo apt-get remove --purge gnome-mines
 	sudo apt-get autoremove
 fi
 
 if command -v rhythmbox >/dev/null; then
-	# Removing Rhythmbox.
 	sudo apt-get remove --purge rhythmbox
 	sudo apt-get autoremove
 fi
 
 if command -v thunderbird >/dev/null; then
-	# Removing Thunderbird.
 	sudo apt-get remove --purge thunderbird*
 	sudo apt-get autoremove
 fi
