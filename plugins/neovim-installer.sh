df07d621a1b154fd6c4fd72cc0cd6e5c62d2974e
Jackson Miras <jackmiras@gmail.com>
2017-04-30 00:06:59 -0400

    Updating neovim-installer.sh script

--- plugins/neovim-installer.sh
+++ plugins/neovim-installer.sh
@@ -11,12 +11,12 @@ else
   # Installing Neovim.
   sudo apt-get install software-properties-common
   sudo apt-get install python-software-properties
-  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo add-apt-repository ppa:neovim-ppa/stable
   sudo apt-get update
   sudo apt-get install neovim
 
   # Installing Python for Neovim.
-  sudo pip install neovim
+  sudo apt-get install python-dev python-pip python3-dev python3-pip
 
   if [ ! -d "$HOME/Projects/neovim-config" ]; then
 	  # Downloading Neovim configs from github.
