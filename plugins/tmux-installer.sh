c78ccf39acd6f4d74fac743aac0aadb5c24075e3
Jackson Miras <jackmiras@gmail.com>
2017-05-03 23:30:58 -0400

    Adding code that get custom settings from github and installs them after installing Tmux

--- plugins/tmux-installer.sh
+++ plugins/tmux-installer.sh
@@ -8,5 +8,20 @@ if command -v tmux >/dev/null; then
 else
 	# Installing Tmux.
 	sudo apt-get update && sudo apt-get install -y tmux
+
+  # Checking if tmux-config folder exists on computer.
+  if [ ! -d "$HOME/Projects/tmux-config" ]; then
+	  # Downloading Tmux configs from github.
+	  cd $HOME/Projects && git clone git@github.com:jackmiras/tmux-config.git && cd $HOME
+  fi
+
+  # Removing any existent .tmux.conf file from $HOME
+  if [ -e "$HOME/.tmux.conf" ]; then
+    rm -rf .tmux.conf
+  fi
+
+  # Creating a symbolic link of the .tmux.config file in the $HOME.
+  ln -s $HOME/Projects/tmux-config/tmux.conf $HOME/
+
 	echo $'\n Tmux was installed with succes.'
 fi
