8558ae7421c826b1f13529c4577ad7a4f3d11cdd
Jackson Miras <jackmiras@gmail.com>
2017-05-04 09:31:08 -0400

    Fixing the indentation of the tmux-installer.sh script

--- plugins/tmux-installer.sh
+++ plugins/tmux-installer.sh
@@ -3,18 +3,18 @@
 # This plugin install and configure Tmux software.
 
 if command -v tmux >/dev/null; then
-	echo $'\n Tmux is already installed'
+  echo $'\n Tmux is already installed'
 else
-	# Installing Tmux.
-	sudo apt-get update && sudo apt-get install -y tmux
+  # Installing Tmux.
+  sudo apt-get update && sudo apt-get install -y tmux
 
   # Checking if tmux-config folder exists on computer.
   if [ ! -d "$HOME/Projects/tmux-config" ]; then
-	  # Downloading Tmux configs from github.
-	  cd $HOME/Projects && git clone git@github.com:jackmiras/tmux-config.git && cd $HOME
+    # Downloading Tmux configs from github.
+    cd $HOME/Projects && git clone git@github.com:jackmiras/tmux-config.git && cd $HOME
   fi
 
-  # Removing any existent .tmux.conf file from $HOME
+  # Removing any existent .tmux.conf file from $HOME.
   if [ -e "$HOME/.tmux.conf" ]; then
     rm -rf .tmux.conf
   fi
@@ -22,5 +22,5 @@ else
   # Creating a symbolic link of the .tmux.config file in the $HOME.
   ln -s $HOME/Projects/tmux-config/tmux.conf $HOME/
 
-	echo $'\n Tmux was installed with succes.'
+  echo $'\n Tmux was installed with succes.'
 fi
