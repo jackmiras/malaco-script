2e180691492a667c99f62d95464aa558c33a958c
Jackson Miras <jackmiras@gmail.com>
2017-05-08 17:01:34 -0400

    Removing unnecessary code from java8-installer.sh script

--- plugins/java8-installer.sh
+++ plugins/java8-installer.sh
@@ -10,8 +10,6 @@ else
   sudo apt-get -y install oracle-java8-installer
   # Setting Oracle Java 8 as default JDK.
   sudo update-java-alternatives -s java-8-oracle && sudo apt-get -y install oracle-java8-set-default
-  # Removing Open JDK 7.
-  sudo apt-get -y purge openjdk-7-jre openjdk-7-jre-lib openjdk-7-jre-headless
 
   echo $'\n Java 8 was installed with success!'
 fi
