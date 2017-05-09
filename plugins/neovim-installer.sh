5af3bf1a7e6c3506301c8df4f1718093252b8e9a
Jackson Miras <jackmiras@gmail.com>
2017-05-09 00:07:55 -0400

    Changing the repository of neovim on neovim-installer.sh script

--- plugins/neovim-installer.sh
+++ plugins/neovim-installer.sh
@@ -12,7 +12,7 @@ else
   sudo apt-get install python-dev python-pip python3-dev python3-pip
   sudo apt-get install software-properties-common
   sudo apt-get install python-software-properties
-  sudo add-apt-repository ppa:neovim-ppa/stable
+  sudo add-apt-repository ppa:neovim-ppa/unstable
   sudo apt-get update
   sudo apt-get install neovim
 
