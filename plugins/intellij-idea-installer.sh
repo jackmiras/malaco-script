dd53cfb1c1e47fad26cf94be7dc841243914a3d7
Jackson Miras <jackmiras@gmail.com>
2017-12-04 14:39:35 -0300

    Updating the version of IntelliJ-IDEA at intellij-idea-installer.sh file

--- plugins/intellij-idea-installer.sh
+++ plugins/intellij-idea-installer.sh
@@ -9,10 +9,10 @@ else
   sudo apt-get install lib32z1 lib32ncurses5 lib32stdc++6
 
   # Downloading IntelliJ IDEA.
-  wget -P $HOME/Downloads/ https://download-cf.jetbrains.com/idea/ideaIC-2017.2.tar.gz
+  wget -P $HOME/Downloads/ https://download-cf.jetbrains.com/idea/ideaIC-2017.3.tar.gz
 
   # Extracting IntelliJ IDEA into Download folder.
-  tar zxvf $HOME/Downloads/ideaIC-2017.2.tar.gz -C $HOME/Downloads/
+  tar zxvf $HOME/Downloads/ideaIC-2017.3.tar.gz -C $HOME/Downloads/
 
   # Renaming the folder to a simpler name.
   mv $HOME/Downloads/idea-IC* $HOME/Downloads/idea-IC
@@ -43,47 +43,47 @@ else
 
   if [ -d "$HOME/Projects/intellij-idea-config" ]; then
     # Removing current configurations of IntelliJ IDEA.
-    rm -rf $HOME/.IdeaIC2017.2/config/codestyles
-    rm -rf $HOME/.IdeaIC2017.2/config/colors
-    rm -rf $HOME/.IdeaIC2017.2/config/fileTemplates
-    rm -rf $HOME/.IdeaIC2017.2/config/options/colors.scheme.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/debugger.renderers.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/debugger.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/editor.codeinsight.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/editor.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/find.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/ide.general.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/jdk.table.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/laf.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/other.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/path.macros.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/project.default.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/options/ui.lnf.xml
-    rm -rf $HOME/.IdeaIC2017.2/config/templates
-    rm -rf $HOME/.IdeaIC2017.2/config/plugins
+    rm -rf $HOME/.IdeaIC2017.3/config/codestyles
+    rm -rf $HOME/.IdeaIC2017.3/config/colors
+    rm -rf $HOME/.IdeaIC2017.3/config/fileTemplates
+    rm -rf $HOME/.IdeaIC2017.3/config/options/colors.scheme.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/debugger.renderers.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/debugger.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/editor.codeinsight.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/editor.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/find.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/ide.general.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/jdk.table.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/laf.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/other.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/path.macros.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/project.default.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/options/ui.lnf.xml
+    rm -rf $HOME/.IdeaIC2017.3/config/templates
+    rm -rf $HOME/.IdeaIC2017.3/config/plugins
 
     # Creating a symbolic link of the new IntelliJ IDEA configurations into .IdeaIC folder.
-    ln -s $HOME/Projects/intellij-idea-config/codestyles/ $HOME/.IdeaIC2017.2/config/
-    ln -s $HOME/Projects/intellij-idea-config/colors/ $HOME/.IdeaIC2017.2/config/
-    ln -s $HOME/Projects/intellij-idea-config/fileTemplates/ $HOME/.IdeaIC2017.2/config/
-    ln -s $HOME/Projects/intellij-idea-config/options/colors.scheme.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/debugger.renderers.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/debugger.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/editor.codeinsight.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/editor.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/find.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/ide.general.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/jdk.local.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/laf.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/other.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/path.macros.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/project.default.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/options/ui.lnf.xml $HOME/.IdeaIC2017.2/config/options/
-    ln -s $HOME/Projects/intellij-idea-config/templates/ $HOME/.IdeaIC2017.2/config/
-    ln -s $HOME/Projects/intellij-idea-config/plugins/ $HOME/.IdeaIC2017.2/config/
+    ln -s $HOME/Projects/intellij-idea-config/codestyles/ $HOME/.IdeaIC2017.3/config/
+    ln -s $HOME/Projects/intellij-idea-config/colors/ $HOME/.IdeaIC2017.3/config/
+    ln -s $HOME/Projects/intellij-idea-config/fileTemplates/ $HOME/.IdeaIC2017.3/config/
+    ln -s $HOME/Projects/intellij-idea-config/options/colors.scheme.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/debugger.renderers.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/debugger.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/editor.codeinsight.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/editor.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/find.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/ide.general.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/jdk.local.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/laf.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/other.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/path.macros.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/project.default.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/options/ui.lnf.xml $HOME/.IdeaIC2017.3/config/options/
+    ln -s $HOME/Projects/intellij-idea-config/templates/ $HOME/.IdeaIC2017.3/config/
+    ln -s $HOME/Projects/intellij-idea-config/plugins/ $HOME/.IdeaIC2017.3/config/
   fi
   # Removing IntelliJ IDEA tar.gz file from Download folder.
-  rm -rf $HOME/Downloads/ideaIC-2017.2.tar.gz
+  rm -rf $HOME/Downloads/ideaIC-2017.3.tar.gz
 
   echo $'\n IntelliJ IDEA has been successfully installed.'
 fi
